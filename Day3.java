ded9e30350c48be8fb63042fcd3a1651970025e6
Fuad <my.fuad.adnansyah@gmail.com>
2025-09-10 19:07:58 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,12 @@
+public class KomentarPendek {
+    public static void main(String[] args) {
+        // Ini komentar satu baris
+        System.out.println("Halo, belajar komentar di Java!");
+
+        /* Ini komentar multi-baris
+           Bisa dipakai untuk menjelaskan kode lebih panjang */
+        int x = 10; // variabel x berisi angka 10
+
+        System.out.println("Nilai x adalah: " + x);
+    }
+}
