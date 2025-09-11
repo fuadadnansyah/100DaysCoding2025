8c78486e7532e2ef20b295653cac8b59c3d10369
Fuad <my.fuad.adnansyah@gmail.com>
2025-09-11 22:44:57 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,16 @@
+public class EscapeSequenceExample {
+    public static void main(String[] args) {
+        // \n = pindah baris
+        System.out.println("Nama saya Fuad Adnansyah.\nSaya sedang belajar Java.");
+
+        // \t = tab (spasi lebar)
+        System.out.println("Nama:\tFuad Adnansyah");
+        System.out.println("Asal:\tMamuju, Sulawesi Barat");
+
+        // \" = menampilkan tanda kutip
+        System.out.println("Saya suka bermain \"Badminton\".");
+
+        // \\ = menampilkan tanda backslash
+        System.out.println("Folder project saya ada di: C:\\Users\\Fuad\\Documents\\Java");
+    }
+}
