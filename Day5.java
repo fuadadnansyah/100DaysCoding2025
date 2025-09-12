537c552a7494fe3b4eeaa954bd78b5dd56cbb693
Fuad <my.fuad.adnansyah@gmail.com>
2025-09-12 22:15:19 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        byte b = 100;
+        short s = 30000;
+        int i = 1000000;
+        long l = 1000000000L;
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+    }
+}
