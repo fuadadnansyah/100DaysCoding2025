319540507ead72aa9ddfc14d622ad0f859463529
Fuad <my.fuad.adnansyah@gmail.com>
2025-09-14 22:36:25 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,20 @@
+public class TipeDataCharBoolean {
+    public static void main(String[] args) {
+        // Tipe data char (menyimpan 1 karakter)
+        char hurufAwal = 'F';
+        char hurufKedua = 'A';
+
+        // Tipe data boolean (true/false)
+        boolean lulus = true;
+        boolean rajin = false;
+
+        // Output
+        System.out.println("Contoh Tipe Data Char:");
+        System.out.println("Huruf Awal : " + hurufAwal);
+        System.out.println("Huruf Kedua: " + hurufKedua);
+
+        System.out.println("\nContoh Tipe Data Boolean:");
+        System.out.println("Apakah lulus? " + lulus);
+        System.out.println("Apakah rajin? " + rajin);
+    }
+}
