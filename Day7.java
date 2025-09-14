1185721050152bcc6e3cc809e5db352503ebc9f4
Fuad <my.fuad.adnansyah@gmail.com>
2025-09-14 22:26:41 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,9 @@
+public class TipeDataPecahan {
+    public static void main(String[] args) {
+        float angkaFloat = 3.14f;     // contoh float
+        double angkaDouble = 3.14159; // contoh double
+
+        System.out.println("Nilai Float  : " + angkaFloat);
+        System.out.println("Nilai Double : " + angkaDouble);
+    }
+}
