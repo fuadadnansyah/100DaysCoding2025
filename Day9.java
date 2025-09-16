310693e830af218bf332b618ddbe27688d12de08
Fuad <my.fuad.adnansyah@gmail.com>
2025-09-16 23:07:07 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,19 @@
+public class Variabel {
+    public static void main(String[] args) {
+        // Deklarasi dan inisialisasi variabel
+        int umur = 17;
+        String nama = "Fuad";
+
+        // Cetak nilai awal
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+
+        // Update nilai variabel
+        umur = 18;
+        nama = "Fuad Adnansyah";
+
+        // Cetak nilai setelah di-update
+        System.out.println("Nama baru: " + nama);
+        System.out.println("Umur baru: " + umur);
+    }
+}
